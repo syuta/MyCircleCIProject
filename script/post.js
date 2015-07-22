e1b49ae63b03616e3af25e6145f2e2a9a799015e
nakamura shuta <nakamura.shuta@gmail.com>
2015-07-22 09:44:31 +0900

    add post.js

--- script/post.js
+++ script/post.js
@@ -1,1 +1,10 @@
-console.log("post");
+var fs = require('fs');
+//削除するファイルのパス
+var filePath = process.argv[2];
+
+fs.unlink(filePath, function (err) {
+  if (err) {
+    console.error(err);
+  }
+  console.log('successfully deleted. path:' + filePath);
+});
